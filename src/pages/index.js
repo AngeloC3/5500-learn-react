278bd2e78f6380fcc7f3fdf859b57d68366bf1c3
AngeloC <cataldo.an@northeastern.edu>
2024-02-15 16:23:14 -0500

    feb 15

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,18 @@ import styles from '@/styles/Home.module.css'
 // import Bio from './qcomps/bios'
 // import TodoList from './qcomps/todos'
 // import Gallery from './qcomps/gallery_props'
-import PackingList from './qcomps/props_item'
+// import PackingList from './qcomps/props_item'
+// import List from './qcomps/list_keys_id'
+// import RecipeList from './qcomps/recipes'
+//import Gallery from './qcomps/state'
+//import Form from './qcomps/stuckForm'
+//import FeedbackForm from './qcomps/thankYouCrash'
+import RequestTracker from './qcomps/shoppingCart'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <PackingList />
+        <RequestTracker />
     </div>
   )
 }
